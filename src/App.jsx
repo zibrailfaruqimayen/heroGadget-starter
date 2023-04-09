f5c531ee124683053dcbabe86a27f008bc075aaa
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-04-09 15:24:25 +0600

    file setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import Header from "./components/Header";
 import React from "react";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 const App = () => {
   return (
     <>
       <Header></Header>
-      <Home></Home>
+      <div className="min-h-[calc(100vh-137px)]">
+        <Outlet></Outlet>
+      </div>
       <Footer></Footer>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,28 @@ import "./index.css";
 import App from "./App";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import About from "./components/About";
+import Home from "./components/Home";
+import ErrorPage from "./components/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
-  },
-  {
-    path: "/about",
-    element: <About></About>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/",
+        element: <About></About>,
+      },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
